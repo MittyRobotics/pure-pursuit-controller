2c083b97880f3a07321ff72e58b322694d1112f1
Owen Leather <owenleather22@mittymonarch.com>
2019-10-07 17:38:15 -0700

    Coordinate manager class

---
+++ src/main/java/com/amhsrobotics/purepursuit/CoordinateManager.java
@@ -0,0 +1,12 @@
+package com.amhsrobotics.purepursuit;
+
+public class CoordinateManager {
+	public static double FORWARD_ANGLE = 0;
+	public static double LEFT_ANGLE = -90;
+	public static double RIGHT_ANGLE = 90;
+	public static double BACKWARD_ANGLE = 180;
+
+	public void coordinateTransformation(){
+
+	}
+}
