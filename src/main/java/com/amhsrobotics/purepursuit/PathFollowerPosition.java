9e126879c7acf281f4c22c250400160b8ad9870d
Owen Leather <43189206+OLeather@users.noreply.github.com>
2019-10-31 16:37:44 -0700

    added comments

--- src/main/java/com/amhsrobotics/purepursuit/PathFollowerPosition.java
+++ src/main/java/com/amhsrobotics/purepursuit/PathFollowerPosition.java
@@ -26,34 +26,40 @@ public class PathFollowerPosition {
     private double pathCentricHeading;
     private double trackWidth;
 
+    /**
+     * The coordinate system of the path calculator
+     * <p>
+     * Used to convert robot coordinates to path coordinates.
+     */
     private CoordinateSystem PATH_COORDINATE_SYSTEM = new CoordinateSystem(
             90,
             TurnSign.POSITIVE,
             VectorDirection.POSITIVE_Y,
             VectorDirection.NEGATIVE_X);
 
-    public void setupRobot(double trackWidth){
+    public void setupRobot(double trackWidth) {
         setTrackWidth(trackWidth);
     }
 
-
     /**
      * Updates the path follower's position with the robot's position
-     * @param x         X position of the robot
-     * @param y         Y position of the robot
-     * @param heading   Heading of the robot/
+     * <p>
+     * These coordinates should be in the standardized {@link CoordinateManager} world coordinate system.
+     *
+     * @param x       X position of the robot
+     * @param y       Y position of the robot
+     * @param heading Heading of the robot/
      */
-    public void update(double x, double y, double heading, double leftVelocity, double rightVelocity){
+    public void update(double x, double y, double heading, double leftVelocity, double rightVelocity) {
         this.x = x;
         this.y = y;
         this.heading = heading;
         this.leftVelocity = leftVelocity;
         this.rightVelocity = rightVelocity;
-        Coordinate pathCentricCoordinate = CoordinateManager.getInstance().coordinateTransformation(new Coordinate(x,y,heading),PATH_COORDINATE_SYSTEM);
+        Coordinate pathCentricCoordinate = CoordinateManager.getInstance().coordinateTransformation(new Coordinate(x, y, heading), PATH_COORDINATE_SYSTEM);
         this.pathCentricX = pathCentricCoordinate.getX();
         this.pathCentricY = pathCentricCoordinate.getY();
         this.pathCentricHeading = pathCentricCoordinate.getAngle();
-
     }
 
     public double getX() {
